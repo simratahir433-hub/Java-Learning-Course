e62a8846e2a2236c6f0042a61e70a37c9cc86d89
Simra Tahir <simratahir433@gmail.com>
2025-09-16 22:12:28 +0500

    Added my Java project files

---
+++ src/MyMessage.java
@@ -0,0 +1,12 @@
+public class MyMessage {
+    public static void main(String[] args) throws Exception {
+        // Using println
+        System.out.println("I want to learn coding because it improves problem-solving skills.");
+        System.out.println("Coding allows me to create useful and interesting applications.");
+        
+        // Using print
+        System.out.print("Coding unlocks exciting pathways to innovate and grow in the tech world. ");
+        System.out.print("I also enjoy the creativity it brings. ");
+        System.out.print("Learning coding helps me think logically.");
+    }
+}
---
+++ src/MyProfile.java
@@ -0,0 +1,11 @@
+public class MyProfile {
+    public static void main(String[] args) throws Exception {
+        String name = "Simra";
+        int age = 22;
+        String city = "Chakwal";
+        String favoriteProgrammingLanguage = "CSS";
+
+        System.out.println("My name is " + name + ". I am " + age + " years old. I live in " + city + ".");
+        System.out.println("My favorite programming language is " + favoriteProgrammingLanguage + ".");
+    }
+}
