df852e0b52b310d14ec5d715cbe85eae950cf49d
Simra Tahir <simratahir433@gmail.com>
2025-09-17 22:10:05 +0500

    updated

--- src/input.java
+++ src/input.java
@@ -9,9 +9,20 @@ public class input {
         name= scanner.nextLine();
         System.out.print("Enter your age:");
         age= scanner.nextInt();
-        scanner.close();
+        // scanner.close();
         System.out.println("Your name is: " + name);
         System.out.println("Your age is: " + age);
 
+        //operators
+        int firstnumber;
+        int secondnumber;
+        int sum;
+        System.out.print("Enter first number:");
+        firstnumber= scanner.nextInt();
+        System.out.print("Enter second number:");
+        secondnumber= scanner.nextInt();
+
+        sum= firstnumber + secondnumber;
+        System.out.println("The sum of "+firstnumber+" and "+secondnumber+" is: "+sum);
     }
 }
